443d97753d5d92b244653e9f06bd91f559ffad41
Danny <contact@danfoster.io>
2018-03-04 16:41:54 -0600

    Final FuzzBuzz

---
+++ c-sharp-essentials/fundamentals-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace fundamentals_1 {
+    class Program {
+        static void Main (string[] args) {
+            // Obj #1
+            for (int i = 1; i <= 255; i++) {
+                Console.WriteLine (i);
+            }
+
+            // Obj #2
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine ("FizBuzz");
+                } else if (i % 5 == 0) {
+                    Console.WriteLine ("Buzz");
+                } else if (i % 3 == 0) {
+                    Console.WriteLine ("Fizz");
+                } else {
+                    Console.WriteLine (i);
+                }
+            }
+        }
+    }
+}
