74a9e2dd5b58760186c18f8ea7c448588f78cb4d
Danny <contact@danfoster.io>
2018-03-05 12:29:25 -0600

    Update

--- c-sharp-essentials/puzzles/Program.cs
+++ c-sharp-essentials/puzzles/Program.cs
@@ -7,12 +7,19 @@ namespace puzzles {
         public static void randomArray() {
             List<int> randomArray = new List<int>();
             Random rand = new Random();
+            int sum = 0;
+            int min = 0;
+            int max = 0;
             for (int i = 0; i < 10; i++) {
                 randomArray.Add(rand.Next(5, 25));
+                sum += randomArray[i];
+                if (randomArray[i] > max) {
+                    max = randomArray[i];
+                } else if (randomArray[i] < min) {
+                    min = randomArray[i];
+                }
             }
-            foreach (var number in randomArray) {
-                Console.WriteLine(number);
-            }
+            Console.WriteLine($"Min: {min}, Max: {max}, Sum: {sum}");
         }
         static void Main(string[] args) {
             randomArray();
