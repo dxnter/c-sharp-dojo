5bdd00b85cba83bdc3dfbf92ec02cace8473f933
Danny <contact@danfoster.io>
2018-03-05 12:14:46 -0600

    Updated code

---
+++ c-sharp-essentials/puzzles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace puzzles {
+    class Program {
+
+        public static void randomArray() {
+            List<int> randomArray = new List<int>();
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++) {
+                randomArray.Add(rand.Next(5, 25));
+            }
+            foreach (var number in randomArray) {
+                Console.WriteLine(number);
+            }
+        }
+        static void Main(string[] args) {
+            randomArray();
+        }
+    }
+}
