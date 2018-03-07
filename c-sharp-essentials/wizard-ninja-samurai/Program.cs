d5b7198bf7e7b26cc6d8b448f3f43e2ba8d39b19
Danny <contact@danfoster.io>
2018-03-06 19:15:55 -0600

    Deleted unneccesary copy

--- c-sharp-essentials/wizard-ninja-samurai/Program.cs
+++ c-sharp-essentials/wizard-ninja-samurai/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace wizard_ninja_samurai
+namespace human
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human danny = new Human("Danny");
+            Human robert = new Human("Robert");
+
+            Wizard charles = new Wizard("Charles");
+
+            Ninja cindy = new Ninja("Cindy");
+
+            Samurai sammy = new Samurai("Sammy");
+
+            charles.Heal();
+            charles.Fireball(danny);
+            danny.Attack(robert);
+
+            cindy.Steal(charles);
+            cindy.GetAway();
+
+            sammy.DeathBlow(charles);
+            sammy.DeathBlow(charles);
+            sammy.DeathBlow(charles);
+            sammy.DeathBlow(charles);
+
+            sammy.Meditate();
+
+            int count = Ninja.HowMany();
         }
     }
 }
