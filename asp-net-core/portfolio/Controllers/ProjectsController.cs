1f989c17f498ec3e8c42d6f782a0e38baf5c6973
Danny <contact@danfoster.io>
2018-03-07 15:34:30 -0600

    Portfolio final

---
+++ asp-net-core/portfolio/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio.Controllers {
+    public class ProjectsController : Controller {
+        [HttpGet]
+        [Route("/projects")]
+        public IActionResult Projects() {
+            return View("Projects");
+        }
+    }
+}
