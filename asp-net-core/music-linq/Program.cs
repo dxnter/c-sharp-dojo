779ff81844b666ee0fe979eb4b6b2b74f36f8749
Danny <contact@danfoster.io>
2018-03-12 13:18:05 -0500

    Simple CRUD final

---
+++ asp-net-core/music-linq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JsonData;
+
+namespace ConsoleApplication {
+    public class Program {
+        public static void Main(string[] args) {
+            //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
+
+            //========================================================
+            //Solve all of the prompts below using various LINQ queries
+            //========================================================
+            //There is only one artist in this collection from Mount Vernon, what is their name and age?
+            var fromMountVernon = from artist in Artists
+            where artist.Hometown == "Mount Vernon"
+            select new { artist.RealName, artist.Age };
+
+            foreach (var Artist in fromMountVernon) {
+                System.Console.WriteLine(Artist);
+            }
+            //Who is the youngest artist in our collection of artists?
+            var youngestArtist = from artist in Artists
+            where
+            //Display all artists with 'William' somewhere in their real name
+
+            //Display the 3 oldest artist from Atlanta
+        }
+    }
+}
---
+++ asp-net-core/simple-crud/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DbConnection;
+
+namespace simple_crud {
+    class Program {
+        static void Main(string[] args) {
+            Create("Danny", "Foster", 12);
+            Read();
+        }
+
+        public static void Read() {
+            var users = DbConnector.Query("SELECT * FROM users");
+            foreach (var user in users) {
+                foreach (var prop in user) {
+                    System.Console.WriteLine(prop);
+                }
+            }
+        }
+
+        public static void Create(string first, string last, int number) {
+            DbConnector.Execute($"INSERT INTO users (FirstName, LastName, FavoriteNumber) VALUES ('{first}', '{last}', '{number}')");
+            System.Console.WriteLine($"{first} {last} successfully added to the database!");
+        }
+    }
+}
