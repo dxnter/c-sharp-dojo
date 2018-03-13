6f4ef2241c4c909e7d720373af10837ffa47259b
Danny <contact@danfoster.io>
2018-03-13 12:23:10 -0500

    Create and delete notes

---
+++ asp-net-core/ajaxNotes/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using DbConnection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ajaxNotes.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            List<Dictionary<string, object>> notes = DbConnector.Query("SELECT * FROM notes ORDER BY id DESC");
+            TempData["notes"] = notes;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("New")]
+        public IActionResult New(string Title, string Note) {
+            try {
+                DbConnector.Execute($"INSERT INTO notes (title, note) VALUES ('{Title}', '{Note}')");
+            } catch {
+                TempData["invalidInput"] = "Invalid note input!";
+                return Redirect("/");
+            }
+            return Redirect("/");
+        }
+
+        [HttpGet]
+        [Route("Delete/{id}")]
+        public IActionResult Delete(int id) {
+            DbConnector.Execute($"DELETE FROM notes WHERE id = {id}");
+            return Redirect("/");
+        }
+    }
+}
