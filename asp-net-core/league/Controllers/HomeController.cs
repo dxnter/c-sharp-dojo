89b4f3764058cb764a5211de5ee1e7c3abdd6dae
Danny <contact@danfoster.io>
2018-03-17 23:29:44 -0500

    Improved front end

---
+++ asp-net-core/league/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using league.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace league.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
